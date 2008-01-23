c69e6bf57d08f5eba6d695935b86c2895de12be6
Albert Lash <albert.lash@docunext.com>
2008-01-23 14:47:39 +0000

    mkdir cache

--- config.php
+++ config.php
@@ -1,6 +1,10 @@
 <?php
 
 define('CACHE_DIR',"/dev/shm/punbb/cache/".$_SERVER['SERVER_NAME']);
+
+if(!is_dir('CACHE_DIR')) { 
+    `mkdir -p CACHE_DIR`
+    }
 $conf = '/var/www/etc/punbb/config-'.strtolower($_SERVER['HTTP_HOST']).'.php';
 if(is_file($conf)) {
     include($conf);
