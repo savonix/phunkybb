15897ac39b93520215a4b1d8ae2716890c4fbb48
Albert Lash <albert.lash@docunext.com>
2008-02-08 16:35:11 +0000

    minor bug

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('CACHE_DIR',"/dev/shm/phunkybb/cache/".$_SERVER['SERVER_NAME']);
 define('CONF_DIR',"/var/www/etc/phunkybb");
 
 if(!is_dir('CACHE_DIR')) { 
-    exec('mkdir -p CACHE_DIR');
+    exec("mkdir -p ".CACHE_DIR);
 }
 
 $conf = CONF_DIR.'/config-'.strtolower($_SERVER['HTTP_HOST']).'.php';
