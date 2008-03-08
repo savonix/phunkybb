517111e227820a8e8d6baecc6a250ed63508b8d5
Albert Lash <albert.lash@docunext.com>
2008-03-08 14:04:03 +0000

    unfinished plugins

---
+++ apps/phunkybb/plugins/htmlentities.plugin.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * -File        htmlentities.plugin.php
+ * -License     LGPL (http://www.gnu.org/copyleft/lesser.html)
+ * -Copyright   Savonix Corporation
+ * -Author		Albert Lash
+ */
+
+/**
+ * @package     Nexista Auth
+ * @subpackage  Plugins
+ * @author      Albert Lash
+ */
+ 
+/**
+ * This action parses bbCode into HTML.
+ *
+ * @package     Nexista Auth
+ * @subpackage  Plugins
+ */
+
+class Nexista_HtmlentitiesAction extends Nexista_Action
+{
+
+    /**
+     * Function parameter array
+     *
+     * @var array
+     */
+
+    protected  $params = array(
+        'text' => '', //text to parse
+        'name' => '' //text to parse
+        );
+
+
+    /**
+     * Applies action
+     *
+     * @return  boolean success
+     */
+
+    protected  function main()
+    {
+		$mytext = Nexista_Flow::getByPath($this->params['text']);
+		$name = $this->params['name'];
+        
+        Nexista_Flow::add($name, $new_text);
+        return true;
+
+    }
+} //end class
+
+?>
