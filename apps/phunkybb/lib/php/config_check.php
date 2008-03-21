6ff6d65af9fed3dcd596f2f305d9cc7b783f6019
Albert Lash <albert.lash@docunext.com>
2008-03-21 01:47:57 +0000

    correct path

--- apps/phunkybb/lib/php/config_check.php
+++ apps/phunkybb/lib/php/config_check.php
@@ -5,6 +5,6 @@ $config_cache = NX_PATH_COMPILE."config_cache.php";
 if(is_file($config_cache)) { 
     Nexista_Flow::add("config_cache","true");
     include($config_cache);
-    Nexista_Flow::add("cache_path",$config);
+    Nexista_Flow::add("board_config",$config);
 }
 ?>
