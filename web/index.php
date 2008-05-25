e32645a47c4b8fa38e58b8d0560618b3c1f97ed2
Albert Lash <albert.lash@docunext.com>
2008-05-25 02:44:02 +0000

    nx_builder

--- web/index.php
+++ web/index.php
@@ -20,11 +20,7 @@ define('APP_NAME','phunkybb');
 $server_init = PROJECT_ROOT."/cache/".SERVER_NAME."/".APP_NAME."/".APP_NAME.".php";
 
 
-    
-    
-    
-
-if(!include($nexista_path.'/plugins/builder.php')) {
+if(!include($nexista_path.'/extensions/nx_builder.php')) {
     echo "Error: Unable to load server loader or builder.";
     exit;
 }
@@ -34,9 +30,9 @@ if(!include($nexista_path.'/plugins/builder.php')) {
 
 // Loader not there or manually getting rebuilt? Build it!
 if(!file_exists($server_init) || isset($_GET['nxbin'])) {
-    build_it_now();
+    nexista_build_it_now();
 } else { // Loader is there, check freshness, then either rebuild or include it.
-    check_freshness();
+    nexista_check_freshness();
         include($server_init);
         exit;
 }
