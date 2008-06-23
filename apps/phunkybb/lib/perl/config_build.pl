1330002064d9d7b4a85da88a157504acdec6fea7
Albert Lash <albert.lash@docunext.com>
2008-06-23 18:04:31 +0000

    using a single perl script to build and check for config cache

--- apps/phunkybb/lib/perl/config_build.pl
+++ apps/phunkybb/lib/perl/config_build.pl
@@ -1,13 +1,29 @@
+use XML::Simple;
+use XML::LibXML;
 
+my $xml_file = '/tmp/config_cache.xml';
+my $parser = XML::LibXML->new();
 $flow = Apache2::Aortica::Kernel::Flow->instance();
-$config_barf = $flow->get_value_by_path("/_R_/config_get/config_get")->{config_get}->{config_get};
 
-# dereference the array
-@config_array = @{$config_barf};
+unless ( -e $xml_file ) {
+    $config_barf = $flow->get_value_by_path("/_R_/config_get/config_get")->{config_get}->{config_get};
+    
+    # dereference the array
+    @config_array = @{$config_barf};
+    
+    $config_cache = \@config_array;
+    my $new_hash = {};
+    foreach my $item (@config_array) {
+        $new_hash->{$item->{conf_name}} = $item->{conf_value};
+    }
+    
+    my $xml_str = XMLout($new_hash, RootName => 'board_config', NoAttr => 1, OutputFile => $xml_file);
+} else { 
 
-$config_cache = \@config_array;
-$new_hash = {};
-foreach my $item (@config_array) {
-    $new_hash->{$item->{conf_name}} = $item->{conf_value};
+    $flow->add("config_cache", "true");
 }
 
+
+my $xml = $parser->parse_file( $xml_file  );
+my $node = $flow->{ DOC }->importNode($xml->documentElement());
+$flow->{ ROOT }->appendChild($node);
