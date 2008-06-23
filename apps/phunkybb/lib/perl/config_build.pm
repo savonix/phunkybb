2e718d07851deeb280a6662c0953713a694a65be
Albert Lash <albert.lash@docunext.com>
2008-06-23 15:31:59 +0000

    mini

--- apps/phunkybb/lib/perl/config_build.pm
+++ apps/phunkybb/lib/perl/config_build.pm
@@ -2,11 +2,12 @@
 $flow = Apache2::Aortica::Kernel::Flow->instance();
 $config_barf = $flow->get_value_by_path("/_R_/config_get/config_get")->{config_get}->{config_get};
 
+# dereference the array
 @config_array = @{$config_barf};
 
 $config_cache = \@config_array;
-
 $new_hash = {};
 foreach my $item (@config_array) {
     $new_hash->{$item->{conf_name}} = $item->{conf_value};
 }
+
