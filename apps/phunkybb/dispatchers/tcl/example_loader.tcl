cc1b4fccfe9ef2cc6d5eb16def60d99056f0e447
Albert Lash <albert.lash@docunext.com>
2008-07-10 20:47:10 +0000

    wow its actually starting to work, once the nsxml.c fix was complete

--- apps/phunkybb/dispatchers/tcl/example_loader.tcl
+++ apps/phunkybb/dispatchers/tcl/example_loader.tcl
@@ -1,10 +1,23 @@
 <%
+#global robo_path
+set robo_path "/var/www/dev/robotizeme"
 
 source /var/www/dev/phunkybb/apps/phunkybb/dispatchers/tcl/phunkybb.tcl
 
 %>
 
 <%
-ns_puts [init::start /var/www/dev/phunkybb/config/config.xml]
-%>
 
+
+init::start /var/www/dev/phunkybb/config/config.xml
+
+set gate [ns_queryget nid]
+
+init::process_gate $gate
+
+ns_puts [init::display]
+
+
+ns_puts [init::stop]
+
+%>
--- apps/phunkybb/dispatchers/tcl/phunkybb.tcl
+++ apps/phunkybb/dispatchers/tcl/phunkybb.tcl
@@ -1,1 +1,4 @@
 source /var/www/dev/robotizeme/kernel/init.tcl
+
+
+
