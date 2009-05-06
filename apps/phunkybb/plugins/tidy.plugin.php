87d0817ee9c19a8a8477303fce8f72bf8a645bdf
Albert Lash <albert.lash@docunext.com>
2009-05-06 04:39:25 +0000

    keep it tidy with this new plugin

---
+++ apps/phunkybb/plugins/tidy.plugin.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * -File        tidy.plugin.php
+ * -License     LGPL (http://www.gnu.org/copyleft/lesser.html)
+ * -Copyright   Savonix Corporation
+ * -Author		Albert Lash
+ */
+
+/**
+ * @package     Nexista Auth
+ * @subpackage  Plugins
+ * @author      Albert Lash
+ */
+ 
+/**
+ * This action parses bbCode into HTML.
+ *
+ * @package     Nexista Auth
+ * @subpackage  Plugins
+ */
+
+class Nexista_TidyAction extends Nexista_Action
+{
+
+    /**
+     * Function parameter array
+     *
+     * @var array
+     */
+
+    protected  $params = array(
+        'text' => '', //text to parse
+        'name' => '' //text to parse
+        );
+
+
+    /**
+     * Applies action
+     *
+     * @return  boolean success
+     */
+
+    protected  function main()
+    {
+
+        $tidy = new tidy;
+        $xpath = $this->params['text'];
+        $node = Nexista_Flow::find($xpath);
+        $config = array('output-xhtml'   => true);
+        foreach($node as $item) {
+            $tidy->parseString($item->nodeValue, $config, 'utf8');
+            $tidy->cleanRepair();
+            $item->nodeValue = str_replace('<body>','',$tidy->Body());
+        }
+
+        return true;
+
+    }
+} //end class
+
+?>
